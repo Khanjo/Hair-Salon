dafac935de05d27b9cc0f4ce7da9cc652619cd8a
Joshua Khan <Khanjo@gmail.com>
2023-07-28 09:20:49 -0700

    WIP Stylists Controllers and Views

---
+++ HairSalon/Controllers/StylistController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HairSalon.Controllers
+{
+    public class StylistsController : Controller
+    {
+        private readonly HairSalonContext _db;
+
+        public StylistsController(HairSalonContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Stylist> model = _db.Stylists.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Stylist stylist)
+        {
+            _db.Stylists.Add(stylist);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            Stylist thisStylist = _db.Stylists.Include(Stylist => Stylist.Clients).FirstOrDefault(stylist => stylist.StylistId == id);
+            return View(thisStylist);
+        }
+    }
+}
