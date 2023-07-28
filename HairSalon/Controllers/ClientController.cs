06f5bccc7488202cf73948dd47203c9d43b9c002
Joshua Khan <Khanjo@gmail.com>
2023-07-28 13:39:26 -0700

    add Client Edit, Delete, and Details Views and Edit and Delete Controllers

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -5,11 +5,11 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HairSalon.Controllers
 {
-    public class ClientsConrtoller : Controller
+    public class ClientsController : Controller
     {
         private readonly HairSalonContext _db;
 
-        public ClientsConrtoller(HairSalonContext db)
+        public ClientsController(HairSalonContext db)
         {
             _db = db;
         }
@@ -29,7 +29,43 @@ namespace HairSalon.Controllers
             }
             _db.Clients.Add(client);
             _db.SaveChanges();
-            return RedirectToAction("Index");
+            return RedirectToAction("Details", "Stylists", new { id = client.StylistId });
+        }
+
+        public ActionResult Details(int id)
+        {
+            Client thisClient = _db.Clients.Include(client => client.Stylist).FirstOrDefault(client => client.ClientId == id);
+            return View(thisClient);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+            return View(thisClient);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Client client)
+        {
+            _db.Clients.Update(client);
+            _db.SaveChanges();
+            return RedirectToAction("Details");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            return View(thisClient);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client thisClient = _db.Clients.FirstOrDefault(client => client.ClientId == id);
+            _db.Clients.Remove(thisClient);
+            _db.SaveChanges();
+            return RedirectToAction("Index", "Stylists");
         }
     }
 }
