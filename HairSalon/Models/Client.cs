9abd5bd9c8a20d4574d76e0fea47c6ece9f63c9f
Joshua Khan <Khanjo@gmail.com>
2023-07-28 14:16:06 -0700

    add code for Edit and Delete Client Views and Description Property to Client

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,6 +3,7 @@ namespace HairSalon.Models
     public class Client
     {
         public string Name { get; set; }
+        public string Description { get; set; }
         public int ClientId { get; set; }
         public int StylistId { get; set; }
         public Stylist Stylist { get; set; }
