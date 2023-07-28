58991a2a3f0e1734b176df365e8ff6d4fa70a741
Joshua Khan <Khanjo@gmail.com>
2023-07-28 09:50:26 -0700

    WIP2 Stylists Views and Controllers

--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -3,6 +3,7 @@ namespace HairSalon.Models
     public class Stylist
     {
         public string Name { get; set; }
+        public string Specialty { get; set; }
         public int StylistId { get; set; }
         public List<Client> Clients { get; set; }
     }
